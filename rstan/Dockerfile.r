8269bd0acacfbb81e2d453c097f831c58f3ecd14
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2019-08-20 13:18:48 +0300

    added some missing dependencies

--- rstan/Dockerfile.r
+++ rstan/Dockerfile.r
@@ -6,6 +6,7 @@ RUN mkdir -p /home/rstudio/.R \
   && cp /root/.R/Makevars /home/rstudio/.R/Makevars \
   && apt-get update \
   && apt-get install -y --no-install-recommends \
+      git \
       libxml2-dev \
       libcairo2-dev \
       libsqlite3-dev \
@@ -15,6 +16,9 @@ RUN mkdir -p /home/rstudio/.R \
       libssh2-1-dev \
       unixodbc-dev \
       libsasl2-dev \
+      libcurl4-openssl-dev \
+      libudunits2-dev \
+      libssl-dev \
   && install2.r --error --deps TRUE \
       tidyverse \
       dplyr \
