90a70472c4c46b3779298cf1af0d7d9fbdae0ce9
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2020-12-29 21:13:04 +0200

    Improved buildscript and simplified dockerfile

--- stack-arm/build.sh
+++ stack-arm/build.sh
@@ -1,2 +1,11 @@
 #/usr/bin/env bash
-docker run --rm -it -v $1:/app -v $(pwd):/root/.local/bin paasim/stack-arm 
+# for GHC 8.8.4 and
+# RESOLVER=lts-16.27
+
+docker run --rm -it \
+  -v $1:/app \
+  -v $(pwd)/bin:/root/.local/bin \
+  -v $(pwd)/.stack:/root/.stack \
+  paasim/stack-arm \
+  stack install --resolver lts-16.27 --system-ghc
+
